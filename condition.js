152f70a224fb9307a0aa040f3e7fbb0bdef7ada9
Manzoor <manjur8287580@gmail.com>
2026-03-09 16:17:33 +0530

    1stpractical

---
+++ condition.js
@@ -0,0 +1,48 @@
+
+//codtional statement switch case:
+//*let day = 3 ;
+//switch (day) {
+   // case 1:
+    //console.log("monday");
+   // break;
+   // case 2:
+    //console.log("tuesday");
+    //break;
+    //case 3:
+    //console.log("wednesday");
+    //break;
+    //case 4:
+    //console.log("thursday");
+    //break;
+    //case 5:
+    //console.log("friday");
+    //break;
+    //case 6:
+    //console.log("saturday");
+    //break;
+    //case 7:
+        //console.log("sunday");
+    //break;
+    //default:
+    //console.log("invalid day"); 
+    
+    
+//} */
+ let temperature =25;
+
+if(temperature > 30){
+
+console.log("It's a hot day.");
+
+}else if(temperature > 20){
+
+console.log("It's a warm day.");
+
+} else if(temperature > 10){
+
+console.log("It's a cool day.");
+
+}else{
+
+console.log("It's a cold day.");
+}
